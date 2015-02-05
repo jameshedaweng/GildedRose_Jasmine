228700c4fe898a9b924e85f912619a38b7e41116
jameshedaweng <heda.weng@gmail.com>
2015-02-05 19:17:28 +0100

    Cleaning

--- src/GildedRose.js
+++ src/GildedRose.js
@@ -10,50 +10,42 @@ var GildedRose = function () {
   GildedRose.updateQuality(items);
 };
 GildedRose.updateQuality = function (items) {
-  for (var i = 0; i < items.length; i++) {
-    if ("Aged Brie" != items[i].name && "Backstage passes to a TAFKAL80ETC concert" != items[i].name) {
-      if (items[i].quality > 0) {
-        if ("Sulfuras, Hand of Ragnaros" != items[i].name) {
-          items[i].quality = items[i].quality - 1
+  for (var i = 0; i < items.length; i++){
+    if ("Sulfuras, Hand of Ragnaros" != items[i].name){
+      if ("Aged Brie" != items[i].name && "Backstage passes to a TAFKAL80ETC concert" == items[i].name){
+        if ("Aged Brie" != items[i].name){
+          changeQuality(i, 1);
         }
-      }
-    } else {
-      if (items[i].quality < 50) {
-        items[i].quality = items[i].quality + 1
-        if ("Backstage passes to a TAFKAL80ETC concert" == items[i].name) {
-          if (items[i].sellIn < 11) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
-            }
+        else{
+          if (items[i].sellIn <= 0){
+            items[i].quality = 0;
           }
-          if (items[i].sellIn < 6) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
-            }
+          else if (items[i].sellIn < 6){
+            changeQuality(i, 3);
+          }
+          else if (items[i].sellIn < 11){
+            changeQuality(i, 2);
+          }
+          else{
+            changeQuality(i, 1);
           }
         }
       }
-    }
-    if ("Sulfuras, Hand of Ragnaros" != items[i].name) {
-      items[i].sellIn = items[i].sellIn - 1;
-    }
-    if (items[i].sellIn < 0) {
-      if ("Aged Brie" != items[i].name) {
-        if ("Backstage passes to a TAFKAL80ETC concert" != items[i].name) {
-          if (items[i].quality > 0) {
-            if ("Sulfuras, Hand of Ragnaros" != items[i].name) {
-              items[i].quality = items[i].quality - 1
-            }
-          }
-        } else {
-          items[i].quality = items[i].quality - items[i].quality
+      else{
+        if ("Conjured Mana Cake" != items[i].name){
+          changeQuality(i, -2);
         }
-      } else {
-        if (items[i].quality < 50) {
-          items[i].quality = items[i].quality + 1
+        else{
+          changeQuality(i, -1);
         }
       }
     }
   }
   return items;
+};
+
+GildedRose.changeQuality = function(i, rate){
+  if (items[i].quality <= 50 && items[i].quality > 0){
+    items[i].quality = items[i].quality + rate;
+  }
 };
