38236515114fb229fbc708fe5c402e8a0693661a
jameshedaweng <heda.weng@gmail.com>
2015-02-16 14:05:39 +0100

    clean

--- src/GildedRose.js
+++ src/GildedRose.js
@@ -1,5 +1,4 @@
 var GildedRose = function () {
-  console.log("OMGHAI!");
   var items = [];
   items.push(new Item("+5 Dexterity Vest", 10, 20));
   items.push(new Item("Aged Brie", 2, 0));
@@ -9,39 +8,58 @@ var GildedRose = function () {
   items.push(new Item("Conjured Mana Cake", 3, 6));
   GildedRose.updateQuality(items);
 };
+
 GildedRose.updateQuality = function (items) {
   for (var i = 0; i < items.length; i++){
-    if ("Sulfuras, Hand of Ragnaros" != items[i].name){
-      if ("Aged Brie" != items[i].name && "Backstage passes to a TAFKAL80ETC concert" == items[i].name){
-        if ("Aged Brie" != items[i].name){
+    switch (checkCategory(items[i].name)){
+      case "A"{
+        changeQuality(i, 1);
+        break;
+      }
+      case "B"{
+        if (item[i].sellIn <= 0)
+          item[i].quality = 0;
+        else if (item[i].sellIn < 6)
+          changeQuality(i, 3);
+        else if (item[i].sellIn < 11){
+          changeQuality(i, 2);
+        }
+        else{
           changeQuality(i, 1);
         }
-        else{
-          if (items[i].sellIn <= 0){
-            items[i].quality = 0;
-          }
-          else if (items[i].sellIn < 6){
-            changeQuality(i, 3);
-          }
-          else if (items[i].sellIn < 11){
-            changeQuality(i, 2);
-          }
-          else{
-            changeQuality(i, 1);
-          }
-        }
+        break;
       }
-      else{
-        if ("Conjured Mana Cake" != items[i].name){
-          changeQuality(i, -2);
-        }
-        else{
-          changeQuality(i, -1);
-        }
+      case "C"{
+        changeQuality(i, -2);
+        break;
+      }
+      case "S"{
+        break;
+      }
+      default{
+        changeQuality(i, -1);
+        break;
       }
     }
   }
   return items;
+};
+
+GildedRose.checkCategory = function(name){
+  if (nameCheck(name, "Aged Brie"))
+    return "A";
+  else if (nameCheck(name, "Backstage passes"))
+    return "B";
+  else if (nameCheck(name, "Conjured"))
+    return "C";
+  else if (nameCheck(name, "Sulfuras"))
+    return "S";
+  else
+    return;
+};
+
+GildedRose.nameCheck = function(name, str){
+  return name.indexOf(str) > -1;
 };
 
 GildedRose.changeQuality = function(i, rate){
